7f34df4fc191d608a2306b9caccff6b9d79dc65a
BapL49 <118617986+BapL49@users.noreply.github.com>
2024-11-13 12:29:37 +0100

    début du header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+      <header class="header"></header>
+</template>
+
+<style scoped>
+.header {
+      width: 80%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      padding: var(--padding);
+
+      background-color: var(--secondary);
+
+      border-bottom-left-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import "./assets/main.css";
+import "./assets/css/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createWebHistory, createRouter } from "vue-router";
 import App from "./App.vue";
+import { MotionPlugin } from "@vueuse/motion";
+
+const routes = [{ path: "/", name: "home", component: App }];
+
+const router = createRouter({
+      history: createWebHistory(),
+      routes,
+});
 
 const app = createApp(App);
 
+app.use(MotionPlugin);
 app.use(createPinia());
+app.use(router);
 
 app.mount("#app");
